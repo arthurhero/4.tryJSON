742429c30ed0d4267374f03b0d3f06d575d80a00
Ziwen Chen <arthurhero@Ziwens-MacBook.local>
2016-11-09 20:26:12 -0600

    Initial Commit

---
+++ tryJSON/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tryJSON
+//
+//  Created by Ziwen Chen on 11/9/16.
+//  Copyright © 2016 Ziwen Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
